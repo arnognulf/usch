13a127b194464f411ed93a78740ce940c5445a75
Thomas Eriksson <thomas.eriksson@gmail.com>
2013-09-05 07:35:13 +0200

    Add MIT license blurb.

---
+++ tests.c
@@ -0,0 +1,84 @@
+/*
+ * USCH - The (permutated) tcsh successor
+ * Copyright (c) 2013 Thomas Eriksson 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include "usch.h"
+#include "minunit.h"
+ 
+int tests_run = 0;
+static char * test_strsplit() {
+    char *p_message = NULL;
+    char **pp_out = NULL;
+    char *p_test1 = "foo bar baz";
+    int num_items = 0;
+    num_items = usch_strsplit(p_test1, " ", &pp_out);
+    mu_assert("num_items > 0", num_items > 0);
+    mu_assert("error: pp_out[0] != foo", strcmp(pp_out[0], "foo") == 0);
+    mu_assert("error: pp_out[1] != bar", strcmp(pp_out[1], "bar") == 0);
+    mu_assert("error: pp_out[2] != baz", strcmp(pp_out[2], "baz") == 0);
+    mu_assert("error: pp_out[3] != NULL", pp_out[3] == NULL);
+    free(pp_out);
+    return 0;
+cleanup:
+    free(pp_out);
+    return p_message;
+}
+
+static char * test_strexp()
+{
+    return "not implemented";
+}
+
+static char * test_cd()
+{
+    return "not implemented";
+}
+
+static char * test_whereis()
+{
+    return "not implemented";
+}
+   
+static char * all_tests()
+{
+    char *p_message = NULL;
+     mu_run_test(test_strsplit);
+     return 0;
+}
+int main(int argc, char **argv)
+{
+     char *result = all_tests();
+     if (result != 0)
+     {
+         printf("\033[31m EPIC FAIL!\033[0m %s\n", result);
+     }
+     else
+     {
+         printf("\033[32m A WINNER IS YOU!\033[0m ALL TESTS PASSED\n");
+     }
+     printf(" Tests run: %d\n", tests_run);
+ 
+     return result != 0;
+ }
+
