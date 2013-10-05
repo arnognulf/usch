6bffbce3f6d725ea63e152394cff416d965b6b2b
Thomas Eriksson <thomas.eriksson@gmail.com>
2013-10-05 21:10:12 +0200

    cd finally works

--- src/usch.h
+++ src/usch.h
@@ -171,107 +171,7 @@ static inline int usch_strexp(char *p_in, size_t num_args, char ***ppp_out, char
     return -1;
 }
 #endif // 0
-/**
- * <A short one line description>
- *  
- *  <Longer description>
- *  <May span multiple lines or paragraphs as needed>
- *   
- *   @param  Description of method's or function's input parameter
- *   @param  ...
- *   @return Description of the return value
- *   */
-static inline int usch_cd_impl(size_t num_args, char *p_dummy, ...)
-{
-    int error = 0;
-    glob_t glob_data;
-    va_list p_ap;
-    char *p_dir = NULL;
-    char *p_chdir = NULL;
-    char format[] = "%s";
 
-    printf("%d\n", (int)num_args);
-    if (num_args > 1)
-    {
-        p_dummy = format;
-        va_start(p_ap, p_dummy);
-        p_dir = va_arg(p_ap, char *);
-    }
-    else
-    {
-        p_dir = getenv("HOME");
-    }
-
-    if (glob(p_dir, GLOB_MARK | GLOB_NOCHECK | GLOB_TILDE | GLOB_NOMAGIC | GLOB_BRACE, NULL, &glob_data) == 0)
-    {
-        p_chdir = glob_data.gl_pathv[0];
-    }
-    else
-    {
-        p_chdir = p_dir;
-    }
-    error = chdir(p_chdir);
-
-    switch (error)
-    {
-        case EACCES:
-            {
-                perror("Permission denied.");
-                break;
-            }
-        case EFAULT:
-            {
-                perror("Path points outside your accessible address space.");
-                break;
-            }
-
-        case EIO:
-            {
-                perror("An I/O error occurred.");
-                break;
-            }
-        case ELOOP:
-            {
-                perror("Too many symbolic links were encountered in resolving path.");
-                break;
-            }
-        case ENAMETOOLONG:
-            {
-                perror("path is too long.");
-                break;
-            }
-        case ENOENT:
-            {
-                perror("The file does not exist.");
-                break;
-            }
-        case ENOMEM:
-            {
-                perror("Insufficient kernel memory was available.");
-                break;
-            }
-        case ENOTDIR:
-            {
-                perror("A component of path is not a directory.");
-                break;
-            }
-        default:
-            break;
-    }
-    printf("cd: %s\n", p_chdir);
-    globfree(&glob_data);
-    if (num_args > 0)
-    {
-        va_end(p_ap);
-    }
-    return error;
-}
-#define usch_cd(...) usch_cd_impl(USCH_ARGC(__VA_ARGS__), "", ##__VA_ARGS__)
-#ifndef cd
-#define cd(...) usch_cd(__VA_ARGS__)
-#else
-#warning included header defines cd(), usch_cd() must be called directly
-#endif // cd
 #if 0
 /**
  * <A short one line description>
@@ -408,173 +308,184 @@ struct usch_glob_list_t
 
 static inline int usch_cmd_impl(size_t num_args, char *p_name, ...)
 {
-    va_list p_ap;
-    size_t i, j;
-    char *p_actual_format = NULL;
-    char **pp_argv = NULL;
-    char **pp_orig_argv = NULL;
-    char **pp_globbed_argv = NULL;
-    pid_t child_pid;
-    pid_t w;
-    int child_status = -1;
-    int status = 0;
-    struct usch_glob_list_t *p_glob_list = NULL;
-    struct usch_glob_list_t *p_current_glob_item = NULL;
-    int orig_arg_idx = 0;
-    int num_glob_items = 0;
-    if (p_name == NULL)
-    {
-        return -1;
-    }
+	va_list p_ap;
+	size_t i, j;
+	char *p_actual_format = NULL;
+	char **pp_argv = NULL;
+	char **pp_orig_argv = NULL;
+	char **pp_globbed_argv = NULL;
+	pid_t child_pid;
+	pid_t w;
+	int child_status = -1;
+	int status = 0;
+	struct usch_glob_list_t *p_glob_list = NULL;
+	struct usch_glob_list_t *p_current_glob_item = NULL;
+	int orig_arg_idx = 0;
+	int num_glob_items = 0;
+	if (p_name == NULL)
+	{
+		return -1;
+	}
 
-    pp_orig_argv = calloc(num_args + 1, sizeof(char*));
-    if (pp_orig_argv == NULL)
-    {
-        status = -1;
-        goto end;
-    }
+	pp_orig_argv = calloc(num_args + 1, sizeof(char*));
+	if (pp_orig_argv == NULL)
+	{
+		status = -1;
+		goto end;
+	}
 
-    p_actual_format = calloc(num_args*2, sizeof(char));
+	p_actual_format = calloc(num_args*2, sizeof(char));
 
-    for (i = 0; i < num_args * 2; i += 2)
-    {
-        p_actual_format[i + 0] = '%';
-        p_actual_format[i + 1] = 's';
-    }
-    p_name = p_actual_format;
+	for (i = 0; i < num_args * 2; i += 2)
+	{
+		p_actual_format[i + 0] = '%';
+		p_actual_format[i + 1] = 's';
+	}
+	p_name = p_actual_format;
 
-    va_start(p_ap, p_name);
+	va_start(p_ap, p_name);
 
-    for (i = 0; i < num_args; i++)
-    {
-        pp_orig_argv[i] = va_arg(p_ap, char *);
-    }
+	for (i = 0; i < num_args; i++)
+	{
+		pp_orig_argv[i] = va_arg(p_ap, char *);
+	}
 
-    for (i = 0; i < num_args; i++)
-    {
-        if (strcmp(pp_orig_argv[i], "--") == 0)
-        {
-            orig_arg_idx++;
-            break;
-        }
+	for (i = 0; i < num_args; i++)
+	{
+		if (strcmp(pp_orig_argv[i], "--") == 0)
+		{
+			orig_arg_idx++;
+			break;
+		}
 
-        if (p_current_glob_item == NULL)
-        {
-            p_current_glob_item = calloc(1, sizeof(usch_glob_list_t));
-            if (p_current_glob_item == NULL)
-                goto end;
-            p_glob_list = p_current_glob_item;
-        }
-        else
-        {
-            p_current_glob_item->p_next = calloc(1, sizeof(usch_glob_list_t));
-            if (p_current_glob_item->p_next == NULL)
-                goto end;
-            p_current_glob_item = p_current_glob_item->p_next;
-        }
-        if (glob(pp_orig_argv[i], GLOB_MARK | GLOB_NOCHECK | GLOB_TILDE | GLOB_NOMAGIC | GLOB_BRACE, NULL, &p_current_glob_item->glob_data) != 0)
-        {
-            goto end;
-        }
-        num_glob_items += p_current_glob_item->glob_data.gl_pathc;
-        orig_arg_idx++;
-    }
-    pp_argv = calloc(num_glob_items + num_args - orig_arg_idx + 1, sizeof(char*));
-    p_current_glob_item = p_glob_list;
+		if (p_current_glob_item == NULL)
+		{
+			p_current_glob_item = calloc(1, sizeof(usch_glob_list_t));
+			if (p_current_glob_item == NULL)
+				goto end;
+			p_glob_list = p_current_glob_item;
+		}
+		else
+		{
+			p_current_glob_item->p_next = calloc(1, sizeof(usch_glob_list_t));
+			if (p_current_glob_item->p_next == NULL)
+				goto end;
+			p_current_glob_item = p_current_glob_item->p_next;
+		}
+		if (glob(pp_orig_argv[i], GLOB_MARK | GLOB_NOCHECK | GLOB_TILDE | GLOB_NOMAGIC | GLOB_BRACE, NULL, &p_current_glob_item->glob_data) != 0)
+		{
+			goto end;
+		}
+		num_glob_items += p_current_glob_item->glob_data.gl_pathc;
+		orig_arg_idx++;
+	}
+	pp_argv = calloc(num_glob_items + num_args - orig_arg_idx + 1, sizeof(char*));
+	p_current_glob_item = p_glob_list;
 
-    i = 0;
-    j = 0;
-    while (p_current_glob_item != NULL)
-    {
-        for (j = 0; j < p_current_glob_item->glob_data.gl_pathc; j++, i++)
-        {
-            pp_argv[i] = p_current_glob_item->glob_data.gl_pathv[j];
-        }
-        p_current_glob_item = p_current_glob_item->p_next;
-    }
+	i = 0;
+	j = 0;
+	while (p_current_glob_item != NULL)
+	{
+		for (j = 0; j < p_current_glob_item->glob_data.gl_pathc; j++, i++)
+		{
+			pp_argv[i] = p_current_glob_item->glob_data.gl_pathv[j];
+		}
+		p_current_glob_item = p_current_glob_item->p_next;
+	}
 
-    for (i = orig_arg_idx; i < num_args; i++)
-    {
-        pp_argv[j + i] = pp_orig_argv[i];
-    }
+	for (i = orig_arg_idx; i < num_args; i++)
+	{
+		pp_argv[j + i] = pp_orig_argv[i];
+	}
 
-    child_pid = fork();
-    if ( child_pid == -1 ) {
-        perror("Cannot proceed. fork() error");
-        return 1;
-    }
-    if(child_pid == 0)
-    {
+	if (strcmp(pp_argv[0], "cd") == 0)
+	{
+		if (pp_argv[1] == NULL)
+			chdir(getenv("HOME"));
+		else
+			chdir(pp_argv[1]);
+	}
+	else
+	{
+		child_pid = fork();
+		if ( child_pid == -1 ) {
+			perror("Cannot proceed. fork() error");
+			return 1;
+		}
+		if(child_pid == 0)
+		{
 
-        i = 0;
-        while (pp_argv[i] != NULL)
-        {
-            i++;
-        }
+			i = 0;
+			while (pp_argv[i] != NULL)
+			{
+				i++;
+			}
 
-        int execv_status = execvp(pp_argv[0], pp_argv);
-        fprintf(stderr, "usch: no such file or directory; %s\n", pp_argv[0]);
+			int execv_status = execvp(pp_argv[0], pp_argv);
+			fprintf(stderr, "usch: no such file or directory; %s\n", pp_argv[0]);
 
-        _exit(execv_status);
-    }
-    else
-    {
-        do
-        {
-            w = waitpid(child_pid, &child_status, WUNTRACED | WCONTINUED);
-            if (w == -1)
-            {
-                perror("waitpid");
-                exit(EXIT_FAILURE);
-            }
+			_exit(execv_status);
+		}
+		else
+		{
+			do
+			{
+				w = waitpid(child_pid, &child_status, WUNTRACED | WCONTINUED);
+				if (w == -1)
+				{
+					perror("waitpid");
+					exit(EXIT_FAILURE);
+				}
 
-            if (WIFEXITED(child_status)) 
-            {
-                status = WEXITSTATUS(child_status);
-            }
-            else if (WIFSIGNALED(child_status))
-            {
-                printf("killed by signal %d\n", WTERMSIG(child_status));
-            }
-            else if (WIFSTOPPED(child_status))
-            {
-                printf("stopped by signal %d\n", WSTOPSIG(child_status));
-            }
-            else if (WIFCONTINUED(child_status))
-            {
-                printf("continued\n");
-            }
-        } while (!WIFEXITED(child_status) && !WIFSIGNALED(child_status));
-    }
+				if (WIFEXITED(child_status)) 
+				{
+					status = WEXITSTATUS(child_status);
+				}
+				else if (WIFSIGNALED(child_status))
+				{
+					printf("killed by signal %d\n", WTERMSIG(child_status));
+				}
+				else if (WIFSTOPPED(child_status))
+				{
+					printf("stopped by signal %d\n", WSTOPSIG(child_status));
+				}
+				else if (WIFCONTINUED(child_status))
+				{
+					printf("continued\n");
+				}
+			} while (!WIFEXITED(child_status) && !WIFSIGNALED(child_status));
+		}
+	}
 
 end:
-    if (num_args > 1)
-    {
-        va_end(p_ap);
-    }
-    if (p_glob_list)
-    {
-        p_current_glob_item = p_glob_list;
-        while (p_current_glob_item != NULL)
-        {
-            struct usch_glob_list_t *p_free_glob_item = p_current_glob_item;
+	if (num_args > 1)
+	{
+		va_end(p_ap);
+	}
+	if (p_glob_list)
+	{
+		p_current_glob_item = p_glob_list;
+		while (p_current_glob_item != NULL)
+		{
+			struct usch_glob_list_t *p_free_glob_item = p_current_glob_item;
 
-            p_current_glob_item = p_current_glob_item->p_next;
+			p_current_glob_item = p_current_glob_item->p_next;
 
-            globfree(&p_free_glob_item->glob_data);
-            free(p_free_glob_item);
-        }
-    }
+			globfree(&p_free_glob_item->glob_data);
+			free(p_free_glob_item);
+		}
+	}
 
-    fflush(stdout);
-    free(pp_globbed_argv);
-    free(pp_orig_argv);
-    free(p_actual_format);
+	fflush(stdout);
+	free(pp_globbed_argv);
+	free(pp_orig_argv);
+	free(p_actual_format);
 
-    return status;
+	return status;
 }
 #define USCH_COUNT_ARGS(...) usch_cmd_impl(USCH_ARGC(__VA_ARGS__), "", ##__VA_ARGS__)
 #define usch_cmd(cmd, ...) USCH_COUNT_ARGS(cmd, ##__VA_ARGS__)
+#define cd(...) usch_cmd("cd", ##__VA_ARGS__)
 
 #if NEED_VIM_WORKAROUND
 {
