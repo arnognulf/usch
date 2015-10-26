3ef412c05f6b66cca3030299e36cc8bbe169d430
Thomas Eriksson <thomas.eriksson@gmail.com>
2015-10-26 22:25:57 +0100

    stub crepl_complete.c

---
+++ libcrepl/crepl_complete.c
@@ -0,0 +1,13 @@
+// http://clang.llvm.org/doxygen/group__CINDEX__CODE__COMPLET.html#ga50fedfa85d8d1517363952f2e10aa3bf
+CINDEX_LINKAGE CXCodeCompleteResults* clang_codeCompleteAt  (   CXTranslationUnit   TU,
+        const char *    complete_filename,
+        unsigned    complete_line,
+        unsigned    complete_column,
+        struct CXUnsavedFile *  unsaved_files,
+        unsigned    num_unsaved_files,
+        unsigned    options 
+
+// http://clang.llvm.org/doxygen/structCXUnsavedFile.html
+// 
+// CINDEX_LINKAGE CXTranslationUnit clang_Cursor_getTranslationUnit (   CXCursor        )   
+// how do we get a CXCursor (which can give us an CXTranslationUnit)??? Traverse the tree?
